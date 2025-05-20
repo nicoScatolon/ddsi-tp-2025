9af7aceedb432fa4c308b39c5caa09c235880f97
IgnacioTVila <ivila@frba.utn.edu.ar>
2025-05-20 16:12:06 -0300

    implementación de consumo de API externa y exponer hechos

--- fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteproxy/controllers/HechoController.java
+++ fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteproxy/controllers/HechoController.java
@@ -1,7 +1,9 @@
 package ar.edu.utn.frba.dds.fuenteproxy.controllers;
 
 import ar.edu.utn.frba.dds.fuenteproxy.domain.entities.Hecho;
+import ar.edu.utn.frba.dds.fuenteproxy.services.IHechosService;
 import ar.edu.utn.frba.dds.fuenteproxy.services.impl.HechoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/fuente-proxy")
 public class HechoController {
-
-    private final HechoService hechoService;
+    @Autowired
+    private final IHechosService hechoService;
 
     public HechoController(HechoService hechoService) {
-        this.hechoService = hechoService;
+        this.hechoService = (IHechosService) hechoService;
     }
 
     @GetMapping("/hechos")
--- fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteproxy/services/IHechosService.java
+++ fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteproxy/services/IHechosService.java
@@ -1,7 +1,9 @@
 package ar.edu.utn.frba.dds.fuenteproxy.services;
 
+import ar.edu.utn.frba.dds.fuenteproxy.domain.entities.Hecho;
+
 import java.util.List;
 
 public interface IHechosService {
-    public List<> buscarTodas();
+        List<Hecho> obtenerHechos();
 }
