c81fb8124a73e218f09fbd52fe68ee83cf4c6e0d
nicoScatolon <nscatolon@frba.utn.edu.ar>
2025-10-09 01:10:40 -0300

    vista perfil

--- clienteGrafico/src/main/java/ar/edu/utn/frba/dds/clienteGrafico/controllers/UsuarioController.java
+++ clienteGrafico/src/main/java/ar/edu/utn/frba/dds/clienteGrafico/controllers/UsuarioController.java
@@ -4,6 +4,10 @@ import ar.edu.utn.frba.dds.clienteGrafico.dtos.output.UsuarioOutputDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Controller
 public class UsuarioController {
@@ -22,5 +26,37 @@ public class UsuarioController {
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Registrarse");
         return "usuario/signup";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String perfil(@PathVariable("id") Long id, Model model) {
+        UsuarioOutputDTO usuario = usuarioPrueba(); //Todo obtener el usuario por id
+        Integer edad = calcularEdad(usuario.getFechaNacimiento());
+        model.addAttribute("usuario", usuario); // Todo: Mock, si no coinciden no se puede editar
+        model.addAttribute("perfil", usuario);
+        model.addAttribute("edad", edad);
+        model.addAttribute("titulo", "Perfil");
+        model.addAttribute("editor", true);
+        return "usuario/profile";
+    }
+
+
+    private Integer calcularEdad(LocalDate fechaNacimiento) {
+        if (fechaNacimiento == null) {
+            return null; // o lanzar excepción según tu lógica
+        }
+        LocalDate hoy = LocalDate.now();
+        return Period.between(fechaNacimiento, hoy).getYears();
+    }
+
+
+    private UsuarioOutputDTO usuarioPrueba(){
+        return UsuarioOutputDTO.builder()
+                .nombre("Usuario")
+                .apellido("Usuario")
+                .email("Usuario")
+                .password("Usuario")
+                .fechaNacimiento(LocalDate.now())
+                .build();
     }
 }
