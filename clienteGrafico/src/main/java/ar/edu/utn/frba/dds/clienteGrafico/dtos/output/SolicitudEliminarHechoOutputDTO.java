4a35adeddd9bcf7555d63572fa3ef5e55a4397b2
nicoScatolon <nscatolon@frba.utn.edu.ar>
2025-10-10 15:39:22 -0300

    rechazar solicitudes de un hecho si una ya fue aceptada

--- clienteGrafico/src/main/java/ar/edu/utn/frba/dds/clienteGrafico/dtos/output/SolicitudEliminarHechoOutputDTO.java
+++ clienteGrafico/src/main/java/ar/edu/utn/frba/dds/clienteGrafico/dtos/output/SolicitudEliminarHechoOutputDTO.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SolicitudEliminarHechoOutputDTO {
+    private Long idSolicitud;
     private String razonDeEliminacion;
     private Long hechoId;
     private LocalDateTime fechaCreacion;
