e7be78bd5b3d6953ab56488179557bbe8ba6325b
nicoScatolon <nscatolon@frba.utn.edu.ar>
2025-10-10 15:42:59 -0300

    rechazar solicitudes de un hecho si una ya fue aceptada

--- servicioAgregacion/src/main/java/ar/edu/utn/frba/dds/domain/repository/ISolicitudesEliminacionRepository.java
+++ servicioAgregacion/src/main/java/ar/edu/utn/frba/dds/domain/repository/ISolicitudesEliminacionRepository.java
@@ -5,6 +5,7 @@ import ar.edu.utn.frba.dds.domain.entities.SolicitudesEliminacion.EstadoDeSolici
 import ar.edu.utn.frba.dds.domain.entities.SolicitudesEliminacion.SolicitudEliminarHecho;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,12 @@ public interface ISolicitudesEliminacionRepository extends JpaRepository<Solicit
     List<SolicitudEliminarHecho> findActives();
 
     SolicitudEliminarHecho findByHechoId(Long hechoId);
+
     List<SolicitudEliminarHecho> findAllByHechoId(Long hechoId);
+
+    @Query("SELECT s FROM SolicitudEliminarHecho s WHERE s.hecho.id = :hechoId AND s.id <> :idExcluida")
+    List<SolicitudEliminarHecho> findAllByHechoIdExcludingSolicitud(@Param("hechoId") Long hechoId,
+                                                                    @Param("idExcluida") Long idExcluida);
+
     List<SolicitudEliminarHecho> findByEstado(EstadoDeSolicitud estado);
 }
