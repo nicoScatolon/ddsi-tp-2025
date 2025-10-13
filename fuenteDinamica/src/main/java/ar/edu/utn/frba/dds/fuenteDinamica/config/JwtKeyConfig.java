1950e42e8d66b3ae2a682f8365b91a4ebf6a3eab
IgnacioTVila <ivila@frba.utn.edu.ar>
2025-10-12 22:44:45 -0300

    Pavadas en ServicioUsuarios

---
+++ fuenteDinamica/src/main/java/ar/edu/utn/frba/dds/fuenteDinamica/config/JwtKeyConfig.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.frba.dds.fuenteDinamica.config;
+
+import ar.edu.utn.frba.dds.fuenteDinamica.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class JwtKeyConfig {
+    @Value("${jwt.secret.base64}")
+    private String base64Secret;
+
+    @PostConstruct
+    public void init() {
+        JwtUtil.initFromBase64(base64Secret);
+    }
+}
--- fuenteEstatica/src/main/java/ar/edu/utn/frba/dds/fuenteEstatica/config/SecurityConfig.java
+++ fuenteEstatica/src/main/java/ar/edu/utn/frba/dds/fuenteEstatica/config/SecurityConfig.java
@@ -1,4 +1,41 @@
 package ar.edu.utn.frba.dds.fuenteEstatica.config;
 
+import ar.edu.utn.frba.dds.fuenteEstatica.filters.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import ar.edu.utn.frba.dds.fuenteEstatica.filters.JwtAuthenticationFilter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() { return new JwtAuthenticationFilter();}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/api/fuenteEstatica/hechos").permitAll()
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
